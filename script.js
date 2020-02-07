030ee94375263843e1b60ff00c9ca5a5e02e16e6
Anamika Panna <anamika2028@gmail.com>
2020-02-06 21:30:54 -0500

    Added conditions for generating passwords

--- script.js
+++ script.js
@@ -8,21 +8,44 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-        
-    }
-    return retVal;
-}
-
   passwordText.value = password;
 
 }
 
+function generatePassword() {
+  var length = prompt("How many characters you want?");
+
+  if (length < 8 || length >128) {
+    alert("Please pick a length between 8 to 128 character")
+    return
+  }
+
+  var needNumber = confirm("Do you want numbers?")
+  var Numbers = '0123456789';
+
+  var Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var needLowercase = confirm("Do you want lowercase?")
+  var Lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+  var needSplchar = confirm("Do you want spsecial character");
+  var Splchar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var retVal = "";
+  
+  for (var i = 0, n = Uppercase.length; i < length; ++i) {
+      retVal += Uppercase.charAt(Math.floor(Math.random() * n));
+      if (needNumber) {
+        retVal += Numbers.charAt(Math.floor(Math.random() * n));
+      }
+      if (needLowercase) {
+        retVal += Lowercase.charAt(Math.floor(Math.random() * n));
+      }
+      if (needSplchar) {
+        retVal += Splchar.charAt(Math.floor(Math.random() * n));
+      }
+  }
+  return retVal;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
